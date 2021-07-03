b0fda548d5d3aa4868546ca1594fc6b54fa94eb0
carlitoy <86399272+carlitoy@users.noreply.github.com>
2021-07-03 13:04:24 +0800

    added ZeroDivisionError, changed coding order

--- logic.py
+++ logic.py
@@ -8,7 +8,13 @@ class calc:
 			self.replacing_values()
 			try:
 				self.value= eval(self.newtext) 
-			except SyntaxError or NameError:
+			except SyntaxError:
+				self.e.delete(0,END)
+				self.e.insert(0,'ERROR')
+			except ZeroDivisionError:
+				self.e.delete(0,END)
+				self.e.insert(0,'ERROR')
+			except NameError:
 				self.e.delete(0,END)
 				self.e.insert(0,'ERROR')
 			else:
